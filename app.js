317fcd79583ae19093dfaedc6576c04acf5b8a57
Yuan WU <jjjjjimyuan@gmail.com>
2024-07-14 21:35:09 +0800

    Feat: add listing page

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+const movies = require('./public/jsons/movies.json').results
+//宣告統一變數的URL
+const BASE_IMG_URL = 'https://movie-list.alphacamp.io/posters/'
 
 //tell express 把樣板引擎交給express-handlebars
 app.engine('.hbs',engine({extname: '.hbs'}))
@@ -18,7 +21,7 @@ app.get('/', (req, res) => {
 
 
 app.get('/movies', (req, res) => {
-  res.render('index')
+  res.render('index', { movies, BASE_IMG_URL }) //{ movies, BASE_IMG_URL } 是一個物件，表示將這些變數傳遞給 index 模板
 })
 
 app.get('/movies/:id', (req, res) => {
